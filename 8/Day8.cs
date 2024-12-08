ee8abd914f5d4c6dde4eb424df96c6685769cba7
Morgan G. Davies <MGD4375@Gmail.com>
2024-12-08 11:14:27 +0000

    Day 8 Part 2

--- 8/Day8.cs
+++ 8/Day8.cs
@@ -6,6 +6,7 @@ public static class Day8
     {
         public Position DistanceTo(Position other) => new(other.X - X, other.Y - Y);
         public Position Add(Position other) => new(other.X + X, other.Y + Y);
+        public Position MultipliedBy(int multiplier) => new(X * multiplier, Y * multiplier);
     };
 
     public record Cell(Position Position, char Content);
@@ -52,12 +53,75 @@ public static class Day8
 
             Console.WriteLine($"Part 1: {total}");
         }
+
+        // Part 2
+        {
+            var groups = map
+                .SelectMany(r => r.Select(c => c))
+                .GroupBy(c => c.Content)
+                .Where(c => c.Key != '.');
+
+            var distinctNodePositions = groups.Select(group =>
+                {
+                    var frequency = group.Key;
+                    var items = group.Select(it => it);
+                    var cartesianProduct = items
+                        .SelectMany(itA => items, (itA, itB) => (itA, itB))
+                        .Where(product => product.itA != product.itB);
+
+                    return cartesianProduct
+                        .SelectMany(pair =>
+                        {
+                            var targetLocations = new List<Position>();
+                            targetLocations.Add(pair.itA.Position);
+                            targetLocations.Add(pair.itB.Position);
+                            var lastItemWasInBounds = true;
+                            var multiplier = 0;
+                            var distance = pair.itA.Position.DistanceTo(pair.itB.Position);
+                            while (lastItemWasInBounds)
+                            {
+                                multiplier += 1;
+                                var resonantDistance = distance.MultipliedBy(multiplier);
+                                var targetLocation = pair.itB.Position.Add(resonantDistance);
+                                var targetIsInBounds = IsInBounds(map, targetLocation);
+                                if (targetIsInBounds) targetLocations.Add(targetLocation);
+                                else lastItemWasInBounds = false;
+                            }
+
+                            return targetLocations;
+                        });
+                })
+                .SelectMany(nodePerGroup => nodePerGroup.Select(n => n))
+                .Distinct();
+
+            // PrintPositions(map, distinctNodePositions.ToList());
+
+            var total = distinctNodePositions.Count();
+
+            Console.WriteLine($"Part 2: {total}");
+        }
     }
 
     private static bool IsInBounds(List<List<Cell>> map, Position node)
     {
         var maxX = map.First().Count;
-        var maxY = map.First().Count;
+        var maxY = map.Count;
         return node.X < maxX && node.Y < maxY && node.X >= 0 && node.Y >= 0;
+    }
+
+    private static void PrintPositions(List<List<Cell>> map, List<Position> positions)
+    {
+        var maxX = map.First().Count;
+        var maxY = map.Count;
+
+        for (var y = 0; y < maxY; y++)
+        {
+            for (var x = 0; x < maxX; x++)
+            {
+                Console.Write(positions.Any(p => p == new Position(x, y)) ? "0" : ".");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
